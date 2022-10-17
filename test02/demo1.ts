728ca76c4e7fcff01132f9cfd9b6fc7c2b40efc9
lujiayu <lujiayu@shiqiao.com>
2022-10-17 21:46:53 +0800

    ts中的数据类型

--- test02/demo1.ts
+++ test02/demo1.ts
@@ -6,6 +6,7 @@
  * @FilePath: /tsTest/test02/demo1.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+// 第二天
 // 因为JavaScript是弱类型语言，例如 '10' + 20 = 30 存在隐式转换  对开发者极不友好
 // 未解决这一问题ts孕育而生，在编程中我们定义变量时就已确定好变量的类型，ts为我们提供一下几种数据类型
 // 布尔类型(boolean)
