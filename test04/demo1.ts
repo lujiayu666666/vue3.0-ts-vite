fcaec7d742d23a3ea07d8cd316ec6b3a1c724cd6
lujiayu <lujiayu@shiqiao.com>
2022-10-17 21:49:18 +0800

    es5定义构造函数及继承

--- test04/demo1.ts
+++ test04/demo1.ts
@@ -6,6 +6,6 @@
  * @FilePath: /tsTest/test04/demo1.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
- 
+//第四天
 // 本节主讲es5继承  请转至js文件夹下的demo1.js文件中查看  
 // 此ts文件禁止编译成js    否则js文件内容将会清空
